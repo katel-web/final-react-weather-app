87b3d3e66c639fddb76a3111355502e7918039dc
katel-web <katelemass@gmail.com>
2024-09-28 16:42:16 +0100

    Added link to personal site

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ function App() {
     <div className="App">
       <h1>Weather app</h1>
       <p>
-        This project was coded by Kate Lemass and is open-sourced{" "}
+        This project was coded by{" "}
+        <a href="https://katelemass.com/" target="_blank" rel="noreferrer">
+          Kate Lemass
+        </a>{" "}
+        and is open-sourced{" "}
         <a
           href="https://github.com/katel-web/final-react-weather-app"
           target="_blank"
