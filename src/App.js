285bb00ac55fd641efe0938fab3f44a8768d108e
katel-web <katelemass@gmail.com>
2024-09-28 16:39:26 +0100

    Added link to GitHub

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ function App() {
   return (
     <div className="App">
       <h1>Weather app</h1>
+      <p>
+        This project was coded by Kate Lemass and is open-sourced{" "}
+        <a
+          href="https://github.com/katel-web/final-react-weather-app"
+          target="_blank"
+        >
+          on GitHub
+        </a>
+      </p>
     </div>
   );
 }
