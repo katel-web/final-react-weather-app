f49ee158650b335f1572e3d87e878181811fa92c
katel-web <katelemass@gmail.com>
2024-09-29 16:13:24 +0100

    Imported Bootstrap from npm

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <p>
-        This project was coded by{" "}
-        <a href="https://katelemass.com/" target="_blank" rel="noreferrer">
-          Kate Lemass
-        </a>{" "}
-        and is open-sourced{" "}
-        <a
-          href="https://github.com/katel-web/final-react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          on GitHub
-        </a>
-      </p>
+      <div className="container">
+        <p>
+          This project was coded by{" "}
+          <a href="https://katelemass.com/" target="_blank" rel="noreferrer">
+            Kate Lemass
+          </a>{" "}
+          and is open-sourced{" "}
+          <a
+            href="https://github.com/katel-web/final-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            on GitHub
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
